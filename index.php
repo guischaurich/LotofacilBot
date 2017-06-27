3e57ab05e2c879fb061dd2ac2f27fc94dc874e32
Guilherme Schaurich <guischaurich@gmail.com>
2017-06-26 23:56:30 -0300

    criada classe e dois métodos para conferir jogo

--- index.php
+++ index.php
@@ -5,15 +5,70 @@ Autor: Guilherme Schaurich
 Data: 26/06/2017
 Versão: 1.l
 */
+class confersLotofacil{ 
 
-class confersLoto{
-    private $gameNumbers = ['1','2','3','5','7','9','11','13','15','19','20','21','23','24','25'];
-    private $url = "lotodicas.com.br/api/lotofacil/";
-    private function __construc($gameNumbers){
-        
+    public function __construct(){
+        $result = $this->getLastGame(['1','2','3','5','7','9','11','13','15','19','20','21','23','24','25']);
+
+        echo "Você acertou ".$result["hits"]." números no jogo ".$result["gameNumber"].".";
+
+        $result = $this->getSpecificGame(['1','2','3','5','7','9','11','13','15','19','20','21','23','24','25'],"1526");
+
+        echo "Você acertou ".$result["hits"]." números no jogo ".$result["gameNumber"].".";
+    }
+    
+    public function getLastGame($numbersPlayed){
+        $url = "http://lotodicas.com.br/api/lotofacil/";
+
         $lastGame = file_get_contents($url);
-        var_dump($lastGame);
+
+        $lasGameArray = json_decode($lastGame);
+
+        $gameNumber = $lasGameArray->{"numero"};
+
+        $gameNumbersDrawn = $lasGameArray->{"sorteio"};
+
+        $countNumbersRight = 0;
+
+        foreach ($gameNumbersDrawn as $number) {
+
+            if(in_array($number,$numbersPlayed)){
+
+                $countNumbersRight ++;
+            }
+        } 
+
+        $result = array("hits" => $countNumbersRight, "gameNumber" => $lasGameArray->{"numero"});
+
+        return $result;
+    }
+
+    public function getSpecificGame($numbersPlayed,$gameNumber){
+
+        $url = "http://lotodicas.com.br/api/lotofacil/";
+
+        $lastGame = file_get_contents($url.$gameNumber);
+
+        $lasGameArray = json_decode($lastGame);
+
+        $gameNumber = $lasGameArray->{"numero"};
+
+        $gameNumbersDrawn = $lasGameArray->{"sorteio"};
+
+        $countNumbersRight = 0;
+
+        foreach ($gameNumbersDrawn as $number) {
+            
+            if(in_array($number,$numbersPlayed)){
+
+                $countNumbersRight ++;
+            }
+        } 
+
+        $result = array("hits" => $countNumbersRight, "gameNumber" => $lasGameArray->{"numero"});
+
+        return $result;
     }
 }
 
-$confersLotoGame = new confersLoto();
+$game = new confersLotofacil();
