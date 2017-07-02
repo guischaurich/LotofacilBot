eaddf821932c2dd0202cb904999f2a5da54de3aa
guischaurich <guischaurich@gmail.com>
2017-07-02 15:31:46 -0400

    teste variavel de ambiente de novo

--- index.php
+++ index.php
@@ -95,7 +95,7 @@ $game = new confersLotofacil();
 $game->botToken = $_ENV["TELEGRAM_BOT_TOKEN"];
 $game->chatAdmin = $_ENV["CHAT_ID"];
 if (isset($response["message"])) {
-	if($_ENV("user".$response['message']['from']['id']) == "true"){
+	if($_ENV("user{$response['message']['from']['id']}") == "true"){
 		
 	}else{
 				$game->sendMessage("sendMessage", array('chat_id' => $game->chatAdmin, "text" => 'Ok, números anotados',
@@ -106,7 +106,7 @@ if (isset($response["message"])) {
 		$game->sendMessage("sendMessage", array('chat_id' => $game->chatAdmin, "text" => 'Informe os números que você jogou',
 																					 'reply_markup' => '{"keyboard":[["1","2","3","4","5","6","7","8","9","10"],["11","12","13","14","15","16","17","18","19","20"]],"resize_keyboard":true,"one_time_keyboard":false}'));
 
-		putenv("user".$response['message']['from']['id']."=true");
+		putenv("user{$response['message']['from']['id']}=true");
 	}
 	else
 	{		
