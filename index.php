19fa28eb8caf2e79c88fe90aafed8767ef986f3b
guischaurich <guischaurich@gmail.com>
2017-07-02 14:59:48 -0400

    resposta botao novo jogo

--- index.php
+++ index.php
@@ -96,9 +96,13 @@ $game->botToken = $_ENV["TELEGRAM_BOT_TOKEN"];
 $game->chatAdmin = $_ENV["CHAT_ID"];
 if (isset($response["message"])) {
 
-error_log($response);
-
-$game->sendMessage("sendMessage", array('chat_id' => $game->chatAdmin, "text" => 'retorno'));
+	error_log($response);
+	if($response["message"]["text"] == "/novojogo"){
+		$game->sendMessage("sendMessage", array('chat_id' => $game->chatAdmin, "text" => 'Informe os números que você jogou',
+																					 'reply_markup' => '{"keyboard":[["1","2","3","4","5","6","7","8","9","10"],["11","12","13","14","15","16","17","18","19","20"]],"resize_keyboard":true,"one_time_keyboard":false}'));
+	}else{
+		$game->sendMessage("sendMessage", array('chat_id' => $game->chatAdmin, "text" => 'retorno'));
+	}
 }else{
 
 $game->callGames();
