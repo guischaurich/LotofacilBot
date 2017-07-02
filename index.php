14d01e92165ad19e3db29e575862d75e42d52de9
guischaurich <guischaurich@gmail.com>
2017-07-02 15:16:07 -0400

    testando escrita em variavel de ambiente mais um

--- index.php
+++ index.php
@@ -100,7 +100,7 @@ if (isset($response["message"])) {
 		$game->sendMessage("sendMessage", array('chat_id' => $game->chatAdmin, "text" => 'Informe os números que você jogou',
 																					 'reply_markup' => '{"keyboard":[["1","2","3","4","5","6","7","8","9","10"],["11","12","13","14","15","16","17","18","19","20"]],"resize_keyboard":true,"one_time_keyboard":false}'));
 
-		putenv("user$response['message']['from']['id']") = true;
+		putenv("user".$response['message']['from']['id']) = true;
 	}
 	else
 	{		
