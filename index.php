90db5a11e2b4ca5ab3d16fcbb4cc8be94ef1ed2e
guischaurich <guischaurich@gmail.com>
2017-07-02 20:22:53 -0400

    ajuste menus

--- index.php
+++ index.php
@@ -99,7 +99,12 @@ if (isset($response["message"])) {
 		$archive = fopen($response["message"]["from"]["id"].".txt","a+");
 		
 		$numbers = fgetcsv($archive,";");
-		if(count($numbers) == 15){
+		if($response["message"]["text"] == "mais"){
+				$game->sendMessage("sendMessage", array('chat_id' => $game->chatAdmin, "text" => 'Informe os números que você jogou',
+																						 'reply_markup' => '{"keyboard":[["11","12","13","14","15"],["16","17","18","19","20"]],"resize_keyboard":true,"one_time_keyboard":false}'));
+		}
+		else if(count($numbers) == 15)
+		{
 			$game->sendMessage("sendMessage", array('chat_id' => $game->chatAdmin, "text" => 'Ok, números anotados',
 																				 'reply_markup' => '{"force_reply":true}'));	
 			
@@ -116,7 +121,7 @@ if (isset($response["message"])) {
 		}
 	}else if($response["message"]["text"] == "/novojogo"){
 			$game->sendMessage("sendMessage", array('chat_id' => $game->chatAdmin, "text" => 'Informe os números que você jogou',
-																						 'reply_markup' => '{"keyboard":[["1","2","3","4","5","6","7","8","9","10"],["11","12","13","14","15","16","17","18","19","20"]],"resize_keyboard":true,"one_time_keyboard":false}'));
+																						 'reply_markup' => '{"keyboard":[["1","2","3","4","5"],["6","7","8","9","10"],["mais"]],"resize_keyboard":true,"one_time_keyboard":false}'));
 
 			$archive = fopen($response["message"]["from"]["id"].".txt","a");
 			fwrite($archive, '');
