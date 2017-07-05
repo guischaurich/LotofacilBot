5914e924ba11b8d4e414dedb0706bc6fdb58d022
Guilherme Schaurich <guischaurich@gmail.com>
2017-07-04 23:21:44 -0300

    ajustado menu

--- lotofacil.php
+++ lotofacil.php
@@ -11,7 +11,7 @@ class confersLotofacil{
 	function __construct(){
         $this->keyboardNumbers = '["1","2","3","4","5"],["6","7","8","9","10"],["11","12","13","14","15"],["16","17","18","19","20"],["21","22","23","24","25"]';
         
-        $this->keyboardMenu = '[["Cadastrar jogo","Ultimo Sorteio"],["Outro Sorteio","Excluir Jogo"]]';
+        $this->keyboardMenu = '["Cadastrar jogo","Ultimo Sorteio"],["Outro Sorteio","Excluir Jogo"]';
 
         $this->botToken = $_ENV["TELEGRAM_BOT_TOKEN"];
         
