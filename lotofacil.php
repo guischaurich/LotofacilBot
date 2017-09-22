f4681904873ba1434e376437b0eb869b69ca7475
Guilherme Schaurich <guischaurich@gmail.com>
2017-09-22 16:44:52 -0300

    conexão pg
    ajustando string de conexão

--- lotofacil.php
+++ lotofacil.php
@@ -102,7 +102,7 @@ class confersLotofacil{
     }
 
     private function connectDB($chatId){
-        $con_string = "host=".$this->dbHost."port=5432 dbname=".$this->database." user=".$this->dbUser." password=".$this->dbPass;
+        $con_string = "host=".$this->dbHost." port=5432 dbname=".$this->database." user=".$this->dbUser." password=".$this->dbPass;
         $conn = pg_connect($con_string);  
         $result = pg_query($conn, "select * from jogo");
         
